fee9a57496144aee8125c5aab79d22cd4846846a
Diana Rios <dianariosram@gmail.com>
2023-05-26 10:55:33 -0600

    edited port

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ app.post('/register', ( req, res ) => {
 })
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`server run on port ${3000}`)
+    console.log(`server run on port ${PORT}`)
 })
